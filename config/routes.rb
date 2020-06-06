3fe4e278323ae56c5a9ad3098f1ef1a13f13abff
Abdulmenaf Kasap <abdulmenaf.kasap@gmail.com>
2020-06-06 18:05:58 +0300

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@
 
   root 'home#index'
   # root to: "admin_users#index"
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
